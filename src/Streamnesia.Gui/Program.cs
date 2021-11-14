50b64331dd8d23579ec502a50c57c767fdbadf97
petrspelos <petrspelos@users.noreply.github.com>
2021-11-14 16:27:15 +0100

    Minor Code-Style fix

--- src/Streamnesia.Gui/Program.cs
+++ src/Streamnesia.Gui/Program.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using System;
 
 namespace Streamnesia.Gui
 {
-    class Program
+    static class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
